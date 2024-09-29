b66c009a45ea9bef8b9f11911cbd3f1a4e8421bf
Drsohayb <127496742+Drsohayb@users.noreply.github.com>
2024-09-29 08:26:19 -0700

    Done

--- recensement/recensement/urls.py
+++ recensement/recensement/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todos/', include('todo.urls')),
+    path('', include('todo.urls')),
 
 ]
 
